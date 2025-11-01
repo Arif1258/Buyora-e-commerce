e0b0da59df0451cbec3ccb90bde91ef03a9962db
Arif <arifahmedma490@gmail.com>
2025-11-02 00:06:13 +0530

    with updated files

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             <div>
                 <img className='mb-5 w-32' src={assets.buyora} alt="" />
-                <p className='w-full md:w-2/3 text-gray-600'>Buyora is a modern e-commerce platform built for a fast, secure, and seamless shopping experience. It combines elegant design, powerful features, and smooth performance to make online shopping effortless for everyone. <br /><br /> <b>Developed By - Sk Asif Ahmed</b></p>
+                <p className='w-full md:w-2/3 text-gray-600'>Buyora is a modern e-commerce platform built for a fast, secure, and seamless shopping experience. It combines elegant design, powerful features, and smooth performance to make online shopping effortless for everyone. <br /><br /> <b>Developed By - Sk Arif Ahmed</b></p>
             </div>
             <div>
                 <p className='text-xl font-medium mb-5'>COMPANY</p>
@@ -20,9 +20,9 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
                 <ul className='flex flex-col gap-1 text-gray-600'>
-                    <a href="https://www.github.com/Asiff07/"><li className='cursor-pointer'>Github</li></a>
-                    <li>skasifahmedofficial@gmail.com</li>
-                    <a href="https://www.instagram.com/skasif_ahmed1/"><li className='cursor-pointer'>Instagram</li></a>
+                    <a href="https://github.com/Arif1258"><li className='cursor-pointer'>Github</li></a>
+                    <li>skarifahmedofficial@gmail.com</li>
+                    <a href="https://www.instagram.com/skarif_ahmed/"><li className='cursor-pointer'>Instagram</li></a>
                 </ul>
             </div>
         </div>
